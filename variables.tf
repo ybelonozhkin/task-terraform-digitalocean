22777006b8f7776be21283a0bcc89b139711d398
Yuriy Belonozhkin <info@nightsochi.ru>
2021-03-05 14:33:25 +0300

    Split variables in separate file

---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "do_token" {
+  type = string
+  description  = "Digital Ocean from the Resource section of the task list"
+}
+variable "pvt_key" {
+  type = string
+  description = "Local path to private key to place on new host"
+}
